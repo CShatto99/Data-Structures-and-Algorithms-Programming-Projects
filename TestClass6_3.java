2bdea64a2df5df85a22cb7eed5e0d8c1cbd0373f
Cameron H Shatto <45926493+CShatto99@users.noreply.github.com>
2019-06-03 22:52:24 -0500

    Add files via upload

---
+++ TestClass6_3.java
@@ -0,0 +1,21 @@
+// this project uses recursion to raise integer 'X' to the power of integer 'Y'.
+import java.util.Scanner;
+public class TestClass6_3 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter two integers (X and Y) seperated by spaces "
+					   + "and X will be raised to the power of Y: ");
+		int x = input.nextInt();
+		int y = input.nextInt();
+		input.close();
+		System.out.println();
+		System.out.print(x + "^" + y + " = " + power(x, y));
+	}
+	
+	public static int power(int x, int y) {
+		if(y == 0)
+			return 1;
+		else
+			return(x * power(x, y-1));
+	}
+}
